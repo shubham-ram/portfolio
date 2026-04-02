95f64893bf5bc986dfdf0bab8e41f2503b496840
shubham-ram <shubhamram2k@gmail.com>
2026-04-03 00:21:56 +0530

    feat: update resume link with download support

--- src/constants/portfolio.js
+++ src/constants/portfolio.js
@@ -4,5 +4,8 @@ export const PORTFOLIO_INFO = {
   email: "shubhamram2k@gmail.com",
   github: "https://github.com/shubham-ram",
   linkedin: "https://www.linkedin.com/in/shubhamram/",
-  resume: "#", // Update with your actual resume PDF link
+  resume:
+    "https://drive.google.com/file/d/1ViGdXE9y0xPP9VFJhvLSUTWxF3xWsz4J/view?usp=sharing",
+  resumeDownload:
+    "https://drive.google.com/uc?export=download&id=1ViGdXE9y0xPP9VFJhvLSUTWxF3xWsz4J",
 };
